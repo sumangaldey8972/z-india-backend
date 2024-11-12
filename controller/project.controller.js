7ea8550111f84a2448a2efeae47e6699055b63df
Madan Gopal <madan.gopal@digitalsherpa.ai>
2024-11-12 11:53:53 +0530

    Fixed : Configuration and places_nearby array was not getting saved

--- controller/project.controller.js
+++ controller/project.controller.js
@@ -1,48 +1,62 @@
-const { createProjectQuery, getProjectQuery, editProjectQuery, deleteProjectByIdQuery } = require("../query/project.query")
-
-
+const {
+	createProjectQuery,
+	getProjectQuery,
+	editProjectQuery,
+	deleteProjectByIdQuery,
+} = require("../query/project.query");
 
 const createProjectController = async (req, res, next) => {
-    try {
-        const response = await createProjectQuery(req.body)
-        return res.status(response.statusCode).json(response)
-    } catch (error) {
-        next(error)
-    }
-}
+	try {
+		const response = await createProjectQuery(req.body);
+		return res.status(response.statusCode).json(response);
+	} catch (error) {
+		next(error);
+	}
+};
 
 const getProjectController = async (req, res, next) => {
-    try {
-        const response = await getProjectQuery()
-        return res.status(response.statusCode).json(response)
-    } catch (error) {
-        next(error)
-    }
-}
+	try {
+		const response = await getProjectQuery();
+		return res.status(response.statusCode).json(response);
+	} catch (error) {
+		next(error);
+	}
+};
 
 const editProjectController = async (req, res, next) => {
-    try {
-        const response = await editProjectQuery(req.query.project_id, req.body)
-        return res.status(response.statusCode).json(response)
-    } catch (error) {
-        next(error)
-    }
-}
+	try {
+		// Parse JSON fields that were sent as strings
+		const configuration = JSON.parse(req.body.configuration);
+		const places_nearby = JSON.parse(req.body.places_nearby);
 
+		// Now you can use these as proper arrays/objects
+		const projectData = {
+			...req.body,
+			configuration,
+			places_nearby,
+		};
+
+		// Now you can pass the parsed data to your query function
+		console.log("Project Data:::::", projectData);
+		const response = await editProjectQuery(req.query.project_id, projectData);
+		return res.status(response.statusCode).json(response);
+	} catch (error) {
+		next(error);
+	}
+};
 
 const deleteProjectController = async (req, res, next) => {
-    try {
-        const response = await deleteProjectByIdQuery(req.query.project_id)
-        return res.status(response.statusCode).json(response)
-    } catch (error) {
-        next(error)
-    }
-}
-
+	try {
+		const response = await deleteProjectByIdQuery(req.query.project_id);
+		return res.status(response.statusCode).json(response);
+	} catch (error) {
+		next(error);
+	}
+};
 
 module.exports = {
-    createProjectController,
-    getProjectController,
-    editProjectController,
-    deleteProjectController
-}
+	createProjectController,
+	getProjectController,
+	editProjectController,
+	deleteProjectController,
+};
